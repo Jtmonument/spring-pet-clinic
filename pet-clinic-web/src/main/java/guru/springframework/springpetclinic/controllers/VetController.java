43f5f43811aecb3f8f15c2019deb56de036be535
Jonathan Taylor <jontaylor091@gmail.com>
2022-06-25 22:39:05 -0500

    Refactor request mapping in VetController

--- pet-clinic-web/src/main/java/guru/springframework/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/springpetclinic/controllers/VetController.java
@@ -3,9 +3,10 @@ package guru.springframework.springpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"", "/index", "/index.html"})
     String getVets() {
         return "vets/index";
     }
